e01307aefc6bec62ef273be02b66210548c668ea
Øyvind Aaraas <Oyvind.Aaraas@klp.no>
2016-12-26 01:05:34 +0100

    feat: enable home page register button

--- app/components/modules/Home.js
+++ app/components/modules/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Jumbotron, Button, Grid, Row, Col} from 'react-bootstrap';
+import {LinkContainer} from 'react-router-bootstrap';
 
 export default React.createClass({
   render() {
@@ -9,7 +10,12 @@ export default React.createClass({
           <Row className="show-grid">
             <Col xs={12} md={8}>
               <h1>checkinout</h1>
-              <p><Button bsStyle="primary">Registrer deg gratis i dag!</Button></p>            </Col>
+              <p>
+                <LinkContainer to="/login">
+                  <Button bsStyle="primary">Registrer deg gratis i dag!</Button>
+                </LinkContainer>
+              </p>
+            </Col>
           </Row>
         </Grid>
       </Jumbotron>
